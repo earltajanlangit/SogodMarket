a9ed599d320efdd6b192967805b46984032fde47
Earl Tajanlangit <ts-earl.tajanlangit@rakuten-sec.co.jp>
2024-11-19 16:41:10 +0800

    11/19/2024 updates

--- admin/bookings/view_documents.php
+++ admin/bookings/view_documents.php
@@ -17,46 +17,7 @@ $client_id = isset($_GET['client_id']) ? intval($_GET['client_id']) : 0;
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            
-            <div class="modal-body">
-                <!-- Static Documents -->
-                <div class="row">
-                <?php 
-                $qry = $conn->query("SELECT * FROM documents WHERE client_id = '{$client_id}'");
-                $row = $qry->fetch_assoc(); // Fetching only one row
-                ?>
-                <div class="modal-body">
-                    <div class="row">
-                        <?php if ($row): ?>
-                        <!-- Cedule File -->
-                        <div class="row">
-                            <div class="col-12 pl-4"> <!-- Added padding-left using Bootstrap class -->
-                                <h5>Cedule File</h5>
-                                <img src="/SogodMarket/uploads/documents/<?php echo $row['cedule_file']; ?>" alt="Cedule File" class="img-fluid" />
-                            </div>
-                        </div>
 
-                        <!-- Photo ID File -->
-                        <div class="row mt-4">
-                            <div class="col-12 pl-4"> <!-- Added padding-left using Bootstrap class -->
-                                <h5>Photo ID File</h5>
-                                <img src="/SogodMarket/uploads/documents/<?php echo $row['photo_id_file']; ?>" alt="Photo ID File" class="img-fluid" />
-                            </div>
-                        </div>
-
-                        <!-- Description -->
-                        <div class="col-12 mt-4 pl-4"> <!-- Added padding-left using Bootstrap class -->
-                            <h5>Description</h5>
-                            <p><?php echo htmlspecialchars($row['description']); ?></p>
-                        </div>
-                        <?php else: ?>
-                        <!-- No Documents Found Message -->
-                        <div class="col-12 text-center">
-                            <p class="text-danger">No documents found for this client.</p>
-                        </div>
-                        <?php endif; ?>
-                    </div>
-                </div>
 
 
             </div>
